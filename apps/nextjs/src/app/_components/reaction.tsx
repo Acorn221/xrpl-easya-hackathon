8598e668700efc6eee8cc826e58d5703cbeccd1c
Dmani Barnett <dmani.barnett@gmail.com>
2024-06-08 18:31:37 +0200

    added drawer

--- apps/nextjs/src/app/_components/reaction.tsx
+++ apps/nextjs/src/app/_components/reaction.tsx
@@ -1,23 +1,37 @@
 "use client";
-import { Button } from "@acme/ui/button";
+import { Button } from "@sobrxrpl/ui/button";
 import React, {useEffect} from "react";
+
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@sobrxrpl/ui/drawer"
+
+
 
 export function Game() {
     const [started, setStarted] = React.useState(false);
     const [color, setColor] = React.useState("fff")
     const [colorGeneratedAt, setColorGeneratedAt] = React.useState(0)
     const [reactionTime, setReactionTime] = React.useState(0)
+    const [reactionRemark, setReactionRemark] = React.useState("")
     const generateColor = () => {
-        console.log("")
         setColor(Math.random().toString(16).substr(-6));
         setColorGeneratedAt(Date.now())
     };
+    const [isOpen, setIsOpen] = React.useState(false);
     useEffect(() => {
-        document.body.addEventListener('click', handleStop);
+        window.addEventListener("click", handleStop)
         return () => {
-            document.body.removeEventListener('click', handleStop);
+            window.removeEventListener("click", handleStop)
         }
-    }, [started]);
+    }, [colorGeneratedAt])
     function handleStart() {
         setStarted(true)
         setTimeout(() => {
@@ -25,26 +39,49 @@ export function Game() {
         }, Math.floor(Math.random() * 5000));
     }
     function handleStop() {
-        if(started){
-            console.log("stopped")
+        if(colorGeneratedAt>0){
+            let normedReactionTime=(Date.now()-colorGeneratedAt)/1000
             setStarted(false)
-            setReactionTime((Date.now()-colorGeneratedAt)/1000)
+            setReactionTime(normedReactionTime)
+            setColorGeneratedAt(0)
             setColor("fff")
+            setIsOpen(true)
+            if (normedReactionTime < 0.2)
+                setReactionRemark("Well done! You are super sharp!")
+            if (normedReactionTime >=0.2 && normedReactionTime < 0.6)
+                setReactionRemark("Nice!")
+            if (normedReactionTime >=0.6 && normedReactionTime < 1)
+                setReactionRemark("Are you sober?")
+            if (normedReactionTime >=1)
+                setReactionRemark("Did you fall asleep?")
         }
-
     }
+
   return (
       <div className="flex flex-row rounded-lg  p-4">
         <div className="flex-grow">
                     <div className="flex flex-row gap-4 ">
-
                         {!started && <Button onClick={handleStart}>Start</Button>}
-
                     </div>
             <div className="flex flex-grow h-52 rounded-lg  p-1 m-1" style={{ backgroundColor: `#${color}` }}>
             </div>
-            <p>Reaction time:{reactionTime}</p>
+            <Drawer open={isOpen} onOpenChange={setIsOpen}>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>Your Reaction Time </DrawerTitle>
+                        <DrawerDescription>Reaction time: {reactionTime} seconds</DrawerDescription>
+                        <DrawerDescription>{reactionRemark}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <DrawerClose>
+                            <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
         </div>
       </div>
   );
 }
+
+
