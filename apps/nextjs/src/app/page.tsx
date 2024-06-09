8c1803d47375c0efcea29faf1eccee90d5fbeac5
James <James@arnott.tech>
2024-06-09 06:35:09 +0200

    fix: prod

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 
+import { auth } from "@sobrxrpl/auth";
+
 import logo from "~/../public/xrpl-logo2.png";
 import { api } from "~/trpc/server";
 import { AuthShowcase } from "./_components/auth-showcase";
@@ -13,8 +15,13 @@ import { WalletManager } from "./_components/wallet";
 
 // export const runtime = "edge";
 
-export default function HomePage() {
+export default async function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
+  const session = await auth();
+
+  if (!session) {
+    return null;
+  }
 
   return (
     <>
