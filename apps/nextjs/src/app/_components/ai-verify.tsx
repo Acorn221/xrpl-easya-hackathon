40dae2b2e761fbad1b08e778bc1d8275411671e4
Dmani Barnett <dmani.barnett@gmail.com>
2024-06-09 07:25:35 +0200

    added chat gtp modl

--- apps/nextjs/src/app/_components/ai-verify.tsx
+++ apps/nextjs/src/app/_components/ai-verify.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {use, useState} from "react";
+import { use, useState } from "react";
 
 import type { RouterOutputs } from "@acme/api";
 import { CreatePostSchema } from "@acme/db/schema";
@@ -32,20 +32,18 @@ import {
     CardHeader,
     CardTitle,
 } from "@sobrxrpl/ui/card"
-type MessageType = {
+interface MessageType {
     content: string;
     role: string;
-};
+}
 
 
 
 export function ChallengeCardWithForm() {
-    const profile="My favourite author is Malorie Blackman and my favourite book is Boys don't cry from Malorie Blackman. My hobbies include dodgeball, karate and climbing which I try to do on a weekly basis. I study computer science which I enjoy."
-    // messages: [{ role: 'user', content: `"My favourite author is Malorie Blackman and my favourite book is Boys don't cry from Malorie Blackman. My hobbies include dodgeball, karate and climbing which I try to do on a weekly basis. I study computer science which I enjoy." Based on this statement ask a security question.` },
-    //     { role: 'assistant', content: `What is the title of your favorite book by Malorie Blackman?` },
-    //     { role: 'user', content: `Boys don't cry. Is this correct yes or no?` }
-    const [messages, setMessages] = useState<Array<MessageType>>([
+    const profile = "My favourite author is Malorie Blackman and my favourite book is Boys don't cry from Malorie Blackman."
+    const [messages, setMessages] = useState<MessageType[]>([
     ]);
+    const [answer, setAnswer] = useState("");
 
     const form = useForm({
         schema: CreatePostSchema,
@@ -57,6 +55,13 @@ export function ChallengeCardWithForm() {
         onSuccess: (val) => {
             console.log("Sucess")
             console.log(val)
+            setMessages(
+                [
+
+                    { role: 'assistant', content: val },
+                    ...messages
+                ]
+            )
         },
         onError: (err) => {
             console.error(err);
@@ -65,8 +70,8 @@ export function ChallengeCardWithForm() {
 
     const answerSecurityChallenge = api.verify.submitAnswer.useMutation({
         onSuccess: (val) => {
-            console.log("Sucess")
-            console.log(val)
+
+
         },
         onError: (err) => {
             console.error(err);
@@ -76,59 +81,36 @@ export function ChallengeCardWithForm() {
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Create project</CardTitle>
+                <CardTitle>AI Verify</CardTitle>
                 <CardDescription>Based on your profile collected on sign up an AI generated security question will need to be generated to verify your identity before completing the transaction.</CardDescription>
             </CardHeader>
             <CardContent>
-                {JSON.stringify(createSecurityChallenge)}
-                    {createSecurityChallenge.isSuccess && createSecurityChallenge.data}
-                {createSecurityChallenge.isSuccess && <Form {...form}>
-                    <form
-                        className="flex w-full max-w-2xl flex-col gap-4"
-                        onSubmit={form.handleSubmit((data) => {
-                           console.log(data)
-                            console.log("Hello")
-                        })}
-                    >
-                        <FormField
-                            control={form.control}
-                            name="response"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <Textarea
-                                            placeholder="Tell us your answer...."
-                                            className="resize-none"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit">Hello</Button>
-                    </form>
-
-                </Form>}
+                <Textarea
+                    placeholder="Tell us your answer...."
+                    className="resize-none"
+                    onChange={(e) => setAnswer(e.target.value)}
+                />
+                {/*{JSON.stringify(createSecurityChallenge)}*/}
+                {createSecurityChallenge.isSuccess && createSecurityChallenge.data}
             </CardContent>
             <CardFooter className="flex justify-between">
-                <Button type="submit">Hello</Button>
 
-                {/*<Button variant="outline" onClick={() =>{*/}
-                {/*    setMessages(*/}
-                {/*        [*/}
-                {/*            ...messages,*/}
-                {/*            { role: 'user', content: `${profile} .Based on this statement ask a security question.` }*/}
-                {/*        ]*/}
-                {/*    )*/}
-                {/*    createSecurityChallenge.mutate({ profile: profile })}}>*/}
-                {/*    Generate a security question*/}
-                {/*</Button>*/}
-                {/*{createSecurityChallenge.isSuccess && <Button onClick={()=>answerSecurityChallenge()}>Submit</Button>}*/}
-                {/*<Button onClick={() => answerSecurityChallenge.mutate()}>Submit</Button>*/}
+
+                <Button variant="outline" onClick={() => {
+                    setMessages(
+                        [
+
+                            { role: 'user', content: `${profile} Based on this statement ask a security question.` },
+                            ...messages
+                        ]
+                    )
+                    createSecurityChallenge.mutate({ profile: profile })
+                }}>
+                    Generate a security question
+                </Button>
+
+                <Button onClick={() => answerSecurityChallenge.mutate({ answer: answer, messages: messages })}>Submit</Button>
                 {/*{JSON.stringify(answerSecurityChallenge)}*/}
-
-
             </CardFooter>
         </Card>
     )
@@ -136,4 +118,3 @@ export function ChallengeCardWithForm() {
 
 
 
-// "My favourite author is Malorie Blackman and my favourite book is Boys don't cry from Malorie Blackman. My hobbies include dodgeball, karate and climbing which I try to do on a weekly basis. I study computer science which I enjoy." Based on this statement ask a security question.
